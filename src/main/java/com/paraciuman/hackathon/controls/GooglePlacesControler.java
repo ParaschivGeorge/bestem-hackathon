43fd7920a35ea2a8693bf126f41428b1210bbe7f
Andrei <razvan.ciurea@stud.acs.upb.ro>
2018-04-14 16:33:49 +0300

    places

--- src/main/java/com/paraciuman/hackathon/controls/GooglePlacesControler.java
+++ src/main/java/com/paraciuman/hackathon/controls/GooglePlacesControler.java
@@ -1,4 +1,13 @@
 package com.paraciuman.hackathon.controls;
 
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GooglePlacesControler {
+
+
 }
