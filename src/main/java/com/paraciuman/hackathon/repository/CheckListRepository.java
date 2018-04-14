5c8e6473614020cead40e81cbeda16557099b732
Andrei <razvan.ciurea@stud.acs.upb.ro>
2018-04-14 15:06:09 +0300

    Refactor

---
+++ src/main/java/com/paraciuman/hackathon/repository/CheckListRepository.java
@@ -0,0 +1,9 @@
+package com.paraciuman.hackathon.repository;
+
+import com.paraciuman.hackathon.model.CheckList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface CheckListRepository extends JpaRepository<CheckList, Integer> {
+}
