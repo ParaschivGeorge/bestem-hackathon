1ce0aa0ed492afd0a48428e18a34a2774236a0f4
Paraschiv George <georgegmtt@gmail.com>
2018-04-14 13:19:02 +0300

    friend1

--- src/main/java/model/Friend.java
+++ src/main/java/model/Friend.java
@@ -15,4 +15,31 @@ public class Friend {
     @OneToOne
     @JoinColumn(name =  "id")
     private User user2;
+
+    public Friend() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser1() {
+        return user1;
+    }
+
+    public void setUser1(User user1) {
+        this.user1 = user1;
+    }
+
+    public User getUser2() {
+        return user2;
+    }
+
+    public void setUser2(User user2) {
+        this.user2 = user2;
+    }
 }
