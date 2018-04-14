0b7369c50ccb6f805bec24e3dc3bc91542dd055e
Paraschiv George <georgegmtt@gmail.com>
2018-04-14 16:33:14 +0300

    http

---
+++ src/main/java/com/paraciuman/hackathon/controls/WeatherApiController.java
@@ -0,0 +1,16 @@
+package com.paraciuman.hackathon.controls;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+import org.json.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@RestController
+public class WeatherApiController {
+
+
+}
