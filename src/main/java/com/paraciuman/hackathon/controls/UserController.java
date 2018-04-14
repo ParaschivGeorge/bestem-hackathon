8330310e37f78adb998aa965c69c15c1c47ee5e5
Andrei <razvan.ciurea@stud.acs.upb.ro>
2018-04-15 01:40:14 +0300

    updateAgenda

--- src/main/java/com/paraciuman/hackathon/controls/UserController.java
+++ src/main/java/com/paraciuman/hackathon/controls/UserController.java
@@ -12,34 +12,34 @@ public class UserController {
     private UserRepository userRepository;
 
     @RequestMapping(method = RequestMethod.POST, value = "user/add")
-    public void addUser(@RequestBody final String email){
-        User user = userRepository.findByEmail(email);
-        if(user != null){
+    public void addUser(@RequestBody final User user){
+        if(userRepository.findByEmail(user.getEmail())!= null){
             return;
         }
         userRepository.save(user);
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "user/update")
-    public void updateUser(@RequestBody final String email){
-        User user = userRepository.findByEmail(email);
-        if(user != null){
+    public void updateUser(@RequestBody final User user){
+        //User user = userRepository.findByEmail(email);
+        if(userRepository.findByEmail(user.getEmail()) != null){
             return;
         }
         userRepository.save(user);
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "user/delete/{id}")
-    public void deleteUser(@PathVariable("id") long id){
-        if(userRepository.findById(id) == null){
+    @RequestMapping(method = RequestMethod.DELETE, value = "user/delete/")
+    public void deleteUser(@RequestBody final String email){
+        User user = userRepository.findByEmail(email);
+        if(user == null){
             return;
         }
-        userRepository.deleteById(id);
+        userRepository.deleteById(user.getId());
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "user/get/{id}")
-    public User getUser(@PathVariable("id") long id){
-        User user = userRepository.findById(id);
+    public User getUser(@RequestBody final String email){
+        User user = userRepository.findByEmail(email);
         if(user == null){
             return null;
         }
