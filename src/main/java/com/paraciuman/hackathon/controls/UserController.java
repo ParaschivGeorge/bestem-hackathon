1574acbebe1318160d0cf5717686fcdc2bb87724
Paraschiv George <georgegmtt@gmail.com>
2018-04-15 01:49:19 +0300

    user test

--- src/main/java/com/paraciuman/hackathon/controls/UserController.java
+++ src/main/java/com/paraciuman/hackathon/controls/UserController.java
@@ -11,6 +11,19 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @GetMapping(path = "/user")
+    public User getUserTest() {
+        User user = new User();
+        user.setId(123);
+        user.setEmail("vasile@email.com");
+        user.setAgendas(null);
+        user.setFriends(null);
+        user.setName("Vasile");
+        user.setPictureUrl("https://www.blogintandem.ro/wp-content/uploads/ngg_featured/tourist-sitting-trolltunga-norway.ngsversion.1485378003501.adapt_.1900.1-670x400.jpg");
+
+        return user;
+    }
+
     @RequestMapping(method = RequestMethod.POST, value = "/user/add")
     public void addUser(@RequestBody final User user){
         if(userRepository.findByEmail(user.getEmail())!= null){
