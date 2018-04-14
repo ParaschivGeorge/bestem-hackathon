5d40b76b22f0f66a3d701cdeba6547800258ed82
Andrei Ciurea <andrei.ciurea96@gmail.com>
2018-04-14 20:43:36 +0300

    repair9

--- src/main/java/com/paraciuman/hackathon/repository/PlacesRepository.java
+++ src/main/java/com/paraciuman/hackathon/repository/PlacesRepository.java
@@ -1,9 +1,16 @@
 package com.paraciuman.hackathon.repository;
 
+import com.paraciuman.hackathon.model.Agenda;
 import com.paraciuman.hackathon.model.Place;
+import com.paraciuman.hackathon.model.Preference;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+
 @Component
 public interface PlacesRepository extends JpaRepository<Place, Long> {
+    ArrayList<Preference> findByAgenda(Agenda agenda);
+    void deleteById(long id);
+    Place findById(long id);
 }
--- src/main/java/com/paraciuman/hackathon/repository/PreferenceRepository.java
+++ src/main/java/com/paraciuman/hackathon/repository/PreferenceRepository.java
@@ -1,9 +1,15 @@
 package com.paraciuman.hackathon.repository;
 
+import com.paraciuman.hackathon.model.Agenda;
 import com.paraciuman.hackathon.model.Preference;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+
 @Component
 public interface PreferenceRepository extends JpaRepository<Preference,Long> {
+    ArrayList<Preference> findByAgenda(Agenda agenda);
+    void deleteById(long id);
+    Preference findById(long id);
 }
