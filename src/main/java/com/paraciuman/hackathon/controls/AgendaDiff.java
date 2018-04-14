2f0873e26f4b1c21ef9ad04c3bceabb0beed4655
Paraschiv George <georgegmtt@gmail.com>
2018-04-14 23:50:27 +0300

    agenda diff rdy

--- src/main/java/com/paraciuman/hackathon/controls/AgendaDiff.java
+++ src/main/java/com/paraciuman/hackathon/controls/AgendaDiff.java
@@ -15,7 +15,15 @@ public class AgendaDiff {
 
     public static DiffType agendaDiff(Agenda agenda1, Agenda agenda2) {
 
-        if (agenda)
+        if (agenda1.getLocation() != agenda2.getLocation())
+            return DiffType.CALL_BOTH;
+        if ((agenda1.getStartDate() != agenda2.getStartDate()) ||
+                (agenda1.getEndDate() != agenda2.getEndDate()))
+            return DiffType.CALL_WEATHER;
+        if (agenda1.getPreferences() != agenda2.getPreferences())
+            return DiffType.CALL_PLACES;
+        if (agenda1.getDays() != agenda2.getDays())
+            return DiffType.MODIFIED;
 
         return DiffType.SAME;
     }
