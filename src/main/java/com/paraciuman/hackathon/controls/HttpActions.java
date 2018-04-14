c31aa70d09950499ee2077843ec32b246e7521fc
Andrei <razvan.ciurea@stud.acs.upb.ro>
2018-04-14 19:00:11 +0300

    geocodingapi

--- src/main/java/com/paraciuman/hackathon/controls/HttpActions.java
+++ src/main/java/com/paraciuman/hackathon/controls/HttpActions.java
@@ -30,8 +30,7 @@ public class HttpActions {
 
     public static Loc getLatLong(String location) throws Exception{
         JSONObject object = getHTML("https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=AIzaSyBx8FdZBpJGNhOzNTECBsCTrwUyjnJAbnU");
-        JSONObject obj = object.getJSONObject("geometry");
-        JSONObject latLong = obj.getJSONObject("location");
+        JSONObject latLong = object.getJSONObject("results").getJSONObject("geometry").getJSONObject("location");
         Loc loc = new Loc(latLong.getDouble("lat"), latLong.getDouble("lng"));
         return loc;
     }
