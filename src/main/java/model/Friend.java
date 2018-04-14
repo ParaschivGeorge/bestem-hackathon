b26956e5f04ed87463c2ab249775c9cb628281ee
Paraschiv George <georgegmtt@gmail.com>
2018-04-14 13:12:55 +0300

    friend

--- src/main/java/model/Friend.java
+++ src/main/java/model/Friend.java
@@ -1,4 +1,18 @@
 package model;
 
+import javax.persistence.*;
+
+@Entity
 public class Friend {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @OneToOne
+    @JoinColumn(name =  "id")
+    private User user1;
+
+    @OneToOne
+    @JoinColumn(name =  "id")
+    private User user2;
 }
