d908f7b56b1463c342e93b55faa3d341f9b84983
Andrei Ciurea <andrei.ciurea96@gmail.com>
2018-04-14 12:57:11 +0300

    userModels ryout

--- src/main/java/com/paraciuman/hackathon/TestController.java
+++ src/main/java/com/paraciuman/hackathon/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    String content = "test15";
+    String content = "test15sdasds";
 
     @GetMapping(path="/test")
     public String testString() {
